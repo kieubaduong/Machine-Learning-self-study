169d60202f0af5fd3d58cb6f8ec1a3c1b3d56f78
Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
2023-04-05 14:37:12 +0700

    add marker linestyle

--- matplot/marker_linestyle.py
+++ matplot/marker_linestyle.py
@@ -2,10 +2,21 @@ import matplotlib.pyplot as plt
 
 x = [1, 2, 3, 4, 5]
 y = [2, 4, 1, 3, 5]
+# marker: định dạng của marker (điểm đánh dấu) trên đường vẽ. Ví dụ: 'o' (tròn), '^' (tam giác), '*' (sao)...
+# ls (linestyle): định dạng của đường vẽ. Ví dụ: 'dashed' (đứt), 'dotted' (chấm), 'solid' (liền)...
+# c (color): màu sắc của đường vẽ.
 # ms (marker size): kích thước của marker.
 # mec (marker edge color): màu sắc của viền marker.
 # mew (marker edge width): độ rộng của viền marker.
 # mfc (marker face color): màu sắc của phần bên trong marker.
-plt.plot(x, y, marker = 'o', linestyle = 'dashed', color = 'r', ms = 20, mec = 'g', mew = 5, mfc = 'b')
+# lw (line width): độ rộng của đường vẽ.
+plt.plot(x, y, marker = 'o', ls = 'dashed', c = 'r', ms = 20, mec = 'g', mew = 5, mfc = 'b', lw = 10)
+
+plt.clf() # clear the previous plot
+
+# draw multi lines by plot multiple times
+plt.plot(x)
+plt.plot(y)
 
 plt.show()
+
--- matplot/plotting.py
+++ matplot/plotting.py
@@ -30,14 +30,6 @@ ypoints = np.array([3, 8, 1, 10, 5, 7])
 plt.plot(ypoints)
 plt.clf() # clear the previous plot
 
-x = [1, 2, 3, 4, 5]
-y = [2, 4, 1, 3, 5]
-# ms (marker size): kích thước của marker.
-# mec (marker edge color): màu sắc của viền marker.
-# mew (marker edge width): độ rộng của viền marker.
-# mfc (marker face color): màu sắc của phần bên trong marker.
-plt.plot(x, y, marker = 'o', linestyle = 'dashed', color = 'r', ms = 20, mec = 'g', mew = 5, mfc = 'b')
 
-plt.show()
 
 

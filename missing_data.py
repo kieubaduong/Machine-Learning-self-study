8c6864f7245292bab8b94dc3ccb926219712a24a
Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
2023-04-14 17:44:42 +0700

    add description

--- missing_data.py
+++ missing_data.py
@@ -5,6 +5,9 @@ import numpy as np
 data = pd.read_csv('dataset/missing_data.csv', header=None, delimiter=';')
 
 # strategy can be mean, median, most_frequent
+# mean replace missing values with the mean of the column
+# median replace missing values with the median of the column
+# most_frequent replace missing values with the most frequent value of the column
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 
 # imputer.fit returns the imputer object with the mean values of the columns

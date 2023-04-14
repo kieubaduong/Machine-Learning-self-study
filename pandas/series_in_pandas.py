828d0c363369fa06e23a15b0148b756404758fc4
Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
2023-04-14 10:00:22 +0700

    using key value instead of using two arrays index and data

--- pandas/series_in_pandas.py
+++ pandas/series_in_pandas.py
@@ -4,4 +4,9 @@ a = [1, 2, 3, 4, "string", 5.65]
 
 a = pd.Series(a, index=['a', 'b', 'c', 'd', 'e', 'f'])
 
-print(a)
+# passing key value pair to Series
+calories = {"day1": 420, "day2": 380, "day3": 390}
+
+calories = pd.Series(calories)
+
+print(calories)
